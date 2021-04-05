8f53173d12c02d1c4b72bd2ed18fa8b6c8a1ec6c
HEMANTH KONDAPALLI <k.hemanth6547@gmail.com>
2021-04-06 01:13:02 +0530

    update

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const router = require('./router/router.js');
 app.use(router);
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/taskSLF/dist/taskSLF'));
+app.use(express.static(__dirname + '/dist/taskSLF'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/taskSLF/dist/taskSLF/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/taskSLF/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
